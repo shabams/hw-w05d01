3d2569846e7ba4dae8245010b780e30e289619ea
AlShabibi <shabams@gmail.com>
2019-10-27 23:43:47 +0300

    Solved

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Movie from "./Movie.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie
+          title="The Fellowship of the Ring"
+          hours="2"
+          minutes="58"
+        ></Movie>
+        <Movie title="The Two Towers" hours="2" minutes="59"></Movie>
+        <Movie title="The Return of the King" hours="3" minutes="21"></Movie>
+      </div>
+    );
+  }
 }
 
 export default App;
